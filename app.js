abb0eb84cb6cf568a97a3102e9b1c6919b868778
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-03 22:29:06 -0300

    exercicios aula 01 de fiap

---
+++ app.js
@@ -0,0 +1,13 @@
+// alert("Hello world!")
+
+// alert("Seu nome é " + nome)
+
+// let resultado = confirm("Você deseja seguir?")
+
+// console.log(`Olá ${nome}, tudo bem?`)
+
+let nome = prompt("Digite seu nome: ")
+let idade = prompt("Digite sua idade: ")
+let profissao = prompt("Digite sua profissão: ")
+
+alert(`Olá ${nome}, você tem ${idade} anos e trabalha com ${profissao}`)
